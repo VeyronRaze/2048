dda144a3f884717ca98384bedc006862c1d72363
VeyronRaze <neeeee502@gmail.com>
2021-03-04 22:13:55 +0200

    Fixed color distribution and test code

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -1,5 +1,6 @@
 #include "Board.h"
 #include <cmath>
+#include <random>
 
 Board::Board(){
 	InitSpawn();
@@ -12,22 +13,28 @@ void Board::Draw(Graphics &gfx, Font ft){
 }
 
 void Board::InitSpawn(){
+	std::mt19937 rng(std::random_device{}());
+	std::uniform_int_distribution<int> uid(0, 11);
 	for(int y = 0; y < height; y++){
 		cells.push_back({});
 		for(int x = 0; x < width; x++)
-			cells[y].push_back({{x, y}, 2048});
+			cells[y].push_back({{x, y}, (int)exp2(uid(rng))});
 	}
 }
 
 void Board::Cell::Draw(Graphics &gfx, Font ft, Vei2 boardPos){
 	Vei2 drawPos = pos * cellSize + boardPos;
-	int nSymbols = 0, temp = value;
-	while(temp > 0){
-		nSymbols++;
-		temp /= 10;
+	if(value != 0){
+		int nSymbols = 0, temp = value;
+		while(temp > 0){
+			nSymbols++;
+			temp /= 10;
+		}
+		gfx.DrawFilledRectBorder(drawPos, {cellSize, cellSize}, Colors::MakeRGB(220, 220 - 10 * (int)log(value), 0), Colors::LightGray);
+		ft.DrawText(std::to_string(value), drawPos + Vei2(50 - 8 * nSymbols, 36), Colors::White, gfx);
 	}
-	gfx.DrawFilledRectBorder(drawPos, {cellSize, cellSize}, Colors::MakeRGB(110 + 5 * (int)log2(value), 110, 0), Colors::LightGray);
-	ft.DrawText(std::to_string(value), drawPos + Vei2(50 - 8 * nSymbols, 36), Colors::White, gfx);
+	else
+		gfx.DrawFilledRectBorder(drawPos, {cellSize, cellSize}, Colors::Gray, Colors::LightGray);
 }
 
 Board::Cell::Cell(Vei2 _pos, int _value) : 
